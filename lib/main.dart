be44b0ddb126f09c72674640553712b48a3177d5
Sangram <sangramharihar007@gmail.com>
2023-02-13 10:02:38 +0530

    web build

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,14 @@ import 'dart:async';
 Future<void> main() async{
 
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyC5CxaajynOJNpiyokk3EYa7C1o0QzOBhQ",
+      appId: "1:723055122752:android:0565e1598d4f61f69742a5",
+      messagingSenderId: "723055122752",
+      projectId: "demotodo-softareo",
+    ),
+  );
 
 
   runApp(MyApp());
